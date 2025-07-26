b25035ff093dfc589138dc551607032984c7c0c2
arashms1994 <arash.ms1994@gmail.com>
2025-07-26 17:28:56 +0330

    Feat: Add CashForm

--- src/components/cashForm/CashForm.tsx
+++ src/components/cashForm/CashForm.tsx
@@ -1,24 +1,76 @@
-import { TextField } from "@mui/material";
+import { Stack, TextField, Typography } from "@mui/material";
+import FileUploader from "../file-uploader/FileUploader";
 
 const CashForm = () => {
   return (
-    <form className={"width:350px"}>
-      <TextField fullWidth id="count" label="مبلغ" variant="standard" />
+    <Stack justifyContent={"center"} alignItems={"center"}>
+      <form style={{ width: "350px" }}>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          padding={"8px"}
+        >
+          <label>
+            <Typography variant="subtitle1" gutterBottom>
+              مبلغ واریز(ريال)
+            </Typography>
+          </label>
+          <TextField type="text" id="count" label="مبلغ" variant="outlined" />
+        </Stack>
 
-      <TextField
-        fullWidth
-        id="due_date"
-        label="تاریخ واریز"
-        variant="standard"
-      />
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          padding={"8px"}
+        >
+          <label>
+            <Typography variant="subtitle1" gutterBottom>
+              تاریخ واریز{" "}
+            </Typography>
+          </label>
+          <TextField
+            type="text"
+            id="due_date"
+            label="تاریخ واریز"
+            variant="outlined"
+          />
+        </Stack>
 
-      <TextField
-        fullWidth
-        id="reference_number"
-        label="شماره مرجع"
-        variant="standard"
-      />
-    </form>
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          width={"100%"}
+          padding={"8px"}
+        >
+          <label>
+            <Typography variant="subtitle1" gutterBottom>
+              شماره مرجع{" "}
+            </Typography>
+          </label>
+          <TextField
+            type="text"
+            id="reference_number"
+            label="شماره مرجع"
+            variant="outlined"
+          />
+        </Stack>
+
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          width={"100%"}
+        >
+          <label>
+            <Typography variant="subtitle1" gutterBottom>
+              آپلود رسید واریز
+            </Typography>
+          </label>
+          <FileUploader orderNumber="52555" subFolder="55555" />
+        </Stack>
+      </form>
+    </Stack>
   );
 };
 
