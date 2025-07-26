61c25621240e448c4dec8313c2d930c9707e81ff
arashms1994 <arash.ms1994@gmail.com>
2025-07-26 12:56:07 +0330

    Initialize

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import QueryProvider from "./providers/QueryProvider.tsx";
+import { CacheProvider, ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import theme from "./theme/theme.ts";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryProvider>
+      <BrowserRouter basename="/SitePages/CashFrom.aspx">
+        <CacheProvider value={cacheRtl}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+        </CacheProvider>
+      </BrowserRouter>
+    </QueryProvider>
+  </StrictMode>
+);
