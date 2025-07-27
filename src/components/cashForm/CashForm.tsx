4f996b70857a5c2e9091ebaaef92b2756e729eb4
arashms1994 <arash.ms1994@gmail.com>
2025-07-27 11:17:32 +0330

    Feat: Add color to textfield

--- src/components/cashForm/CashForm.tsx
+++ src/components/cashForm/CashForm.tsx
@@ -1,4 +1,4 @@
-import { Stack, TextField, Typography } from "@mui/material";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import FileUploader from "../file-uploader/FileUploader";
 
 const CashForm = () => {
@@ -12,59 +12,41 @@ const CashForm = () => {
         >
           <label>
             <Typography variant="subtitle1" gutterBottom>
-              مبلغ واریز(ريال):
-            </Typography>
-          </label>
-          <TextField type="text" id="count" variant="outlined" />
-        </Stack>
+            مبلغ واریز(ريال):
+          </Typography>
+        </label>
+        <TextField color="secondary" type="text" id="count" variant="outlined" />
+      </Stack>
 
-        <Stack
-          direction={"row"}
-          justifyContent={"space-between"}
-          width={"100%"}
-        >
-          <label>
-            <Typography variant="subtitle1" gutterBottom>
-              تاریخ واریز:
-            </Typography>
-          </label>
-          <TextField
-            type="text"
-            id="due_date"
-            variant="outlined"
-          />
-        </Stack>
+      <Stack direction={"row"} justifyContent={"space-between"} width={"100%"}>
+        <label htmlFor="due_date">
+          <Typography variant="subtitle1" gutterBottom>
+            تاریخ واریز:
+          </Typography>
+        </label>
+        <TextField color="secondary" type="text" id="due_date" variant="outlined" />
+      </Stack>
 
-        <Stack
-          direction={"row"}
-          justifyContent={"space-between"}
-          width={"100%"}
-        >
-          <label>
-            <Typography variant="subtitle1" gutterBottom>
-              شماره مرجع:
-            </Typography>
-          </label>
-          <TextField
-            type="text"
-            id="reference_number"
-            variant="outlined"
-          />
-        </Stack>
+      <Stack direction={"row"} justifyContent={"space-between"} width={"100%"}>
+        <label htmlFor="reference_number">
+          <Typography variant="subtitle1" gutterBottom>
+            شماره مرجع:
+          </Typography>
+        </label>
+        <TextField color="secondary" type="text" id="reference_number" variant="outlined" />
+      </Stack>
 
-        <Stack
-          direction={"row"}
-          justifyContent={"space-between"}
-          width={"100%"}
-        >
-          <label>
-            <Typography variant="subtitle1" gutterBottom>
-              آپلود رسید واریز
-            </Typography>
-          </label>
-          <FileUploader orderNumber="52555" subFolder="55555" />
-        </Stack>
-      </form>
+      <Stack direction={"row"} justifyContent={"space-between"} width={"100%"}>
+        <Typography variant="subtitle1" gutterBottom>
+          آپلود رسید واریز
+        </Typography>
+        <FileUploader orderNumber="52555" subFolder="55555" />
+      </Stack>
+
+      <Button color="secondary" variant="contained">
+        ثبت واریز
+      </Button>
+    </form>
     </Stack>
   );
 };
